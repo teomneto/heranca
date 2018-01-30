3dee089cb8dbef1aaf8182fee7d25d2abb6f01b1
php 10 <php.10@salsicha.cec.ime.usp.br>
2018-01-29 22:49:43 -0200

    meu ultimo

--- Funcionario.php
+++ Funcionario.php
@@ -1,6 +1,13 @@
 <?php
 class Funcionario;
+{
+public $matricula;
+public $nome;
+public $salario;
 
-$funcionario->nome;
-
-$funcionario->salario;
+public function calculaBonificacao()
+{
+// no caso do gerente :
+//return this->$salario * 0.6 + 100;
+}
+}
